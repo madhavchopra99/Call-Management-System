0237bdc55d5bc84ffa736cd4d8c78b8fa49c64b2
Madhav Chopra <madhavchopra99@gmail.com>
2020-11-29 23:02:44 +0530

    Bootstrap Added Signed-off-by: Madhav Chopra <madhavchopra99@gmail.com>

--- CMS/calls/views.py
+++ CMS/calls/views.py
@@ -9,87 +9,97 @@ from calls.python_scripts.main import *
 
 def write(ivr_file_entry, redundancy_set, csv_response):
 
-	# this function is to write data in log_file
+    # this function is to write data in log_file
 
-	if ivr_file_entry[4] not in redundancy_set:  # used to check if value preexits in file
+    # used to check if value preexits in file
+    if ivr_file_entry[4] not in redundancy_set:
 
-		ivr_file_entry = list([ivr_file_entry[4], ivr_file_entry[3], ivr_file_entry[8], ivr_file_entry[11], ivr_file_entry[9]])  # create the row to push in logfile
+        ivr_file_entry = list([ivr_file_entry[4], ivr_file_entry[3], ivr_file_entry[8],
+                               ivr_file_entry[11], ivr_file_entry[9]])  # create the row to push in logfile
 
-		# in above ivr_file_entry[4] is mobile number, ivr_file_entry[3] is status, ivr_file_entry[8] is duration, ivr_file_entry[11] is department
+        # in above ivr_file_entry[4] is mobile number, ivr_file_entry[3] is status, ivr_file_entry[8] is duration, ivr_file_entry[11] is department
 
-		redundancy_set.add(ivr_file_entry[0])  # ivr_file_entry[0] is mobile number as it is primary key
-		csv_response.writerow(ivr_file_entry)
+        # ivr_file_entry[0] is mobile number as it is primary key
+        redundancy_set.add(ivr_file_entry[0])
+        csv_response.writerow(ivr_file_entry)
 
 
 def logs(request):
 
-	context = {}
+    context = {}
 
-	if request.method == 'POST':
+    if request.method == 'POST':
 
-		try:
+        try:
 
-			ivr_file = request.FILES['ivr']
-			mobile_file = request.FILES['mobile']
+            ivr_file = request.FILES['ivr']
+            mobile_file = request.FILES['mobile']
 
-			fs = FileSystemStorage()
+            fs = FileSystemStorage()
 
-			if fs.exists('ivr_file.csv'):
-				fs.delete('ivr_file.csv')
+            if fs.exists('ivr_file.csv'):
+                fs.delete('ivr_file.csv')
 
-			if fs.exists('mobile.csv'):
-				fs.delete('mobile.csv')
+            if fs.exists('mobile.csv'):
+                fs.delete('mobile.csv')
 
-			fs.save('ivr_file.csv', ivr_file)
-			fs.save('mobile.csv', mobile_file)
+            fs.save('ivr_file.csv', ivr_file)
+            fs.save('mobile.csv', mobile_file)
 
-			path_ivr = os.path.join(settings.BASE_DIR, 'media/ivr_file.csv')
-			path_mobile = os.path.join(settings.BASE_DIR, 'media/mobile.csv')
-			path_response = os.path.join(settings.BASE_DIR, 'media/response.csv')
+            path_ivr = os.path.join(settings.BASE_DIR, 'media/ivr_file.csv')
+            path_mobile = os.path.join(settings.BASE_DIR, 'media/mobile.csv')
+            path_response = os.path.join(
+                settings.BASE_DIR, 'media/response.csv')
 
-			dump_redundancy_set = main(path_ivr, path_mobile)
+            dump_redundancy_set = main(path_ivr, path_mobile)
 
-			with open(path_ivr) as csv_ivr, open(path_response, 'w') as response:
+            with open(path_ivr) as csv_ivr, open(path_response, 'w') as response:
 
-				csv_ivr_file = csv.reader(csv_ivr)
-				next(csv_ivr_file)
+                csv_ivr_file = csv.reader(csv_ivr)
+                next(csv_ivr_file)
 
-				csv_response = csv.writer(response)
-				csv_response.writerow(['Mobile_Number', 'Status', 'Duration', 'Department (if any)', 'Time'])
+                csv_response = csv.writer(response)
+                csv_response.writerow(
+                    ['Mobile_Number', 'Status', 'Duration', 'Department (if any)', 'Time'])
 
-				redundancy_set = set({})
+                redundancy_set = set({})
 
-				for ivr_file_entry in csv_ivr_file:
+                for ivr_file_entry in csv_ivr_file:
 
-					if ivr_file_entry[4] in dump_redundancy_set:
+                    if ivr_file_entry[4] in dump_redundancy_set:
 
-						write(ivr_file_entry, redundancy_set, csv_response)
+                        write(ivr_file_entry, redundancy_set, csv_response)
 
-			context['result'] = 'success'
+            context['result'] = 'success'
 
-		except Exception as e:
+        except Exception as e:
 
-			context['exception'] = e
-			print(e)
+            context['exception'] = e
+            print(e)
 
-	return render(request, 'index.html', context)
+    return render(request, 'index.html', context)
 
 
 def ivr_histogram(request):
 
-	path_ivr = os.path.join(settings.BASE_DIR, 'media/ivr_file.csv')
-	analytics.analyse(path_ivr)
+    path_ivr = os.path.join(settings.BASE_DIR, 'media/ivr_file.csv')
+    analytics.analyse(path_ivr)
 
-	return render(request, 'ivr_histogram.html')
+    return render(request, 'ivr_histogram.html')
 
 
 def result(request):
 
-	context = {}
-	path_response = os.path.join(settings.BASE_DIR, 'media/response.csv')
-	response = open(path_response)
-	csv_response = csv.reader(response)
+    context = {}
+    path_response = os.path.join(settings.BASE_DIR, 'media/response.csv')
+    with open(path_response) as response:
+        csv_response = csv.reader(response)
+        result = []
 
-	context['result'] = csv_response
+        for row in csv_response:
+            result.append(row)
 
-	return render(request, 'result.html', context)
+    context['heading'] = result.pop(0)
+    context['result'] = result
+
+    return render(request, 'result.html', context)
